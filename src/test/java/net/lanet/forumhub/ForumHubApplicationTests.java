8831d6f9c23209cd55e17cfec475932e8d67c45b
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2024-06-20 23:43:49 -0300

    commit inicial

---
+++ src/test/java/net/lanet/forumhub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lanet.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
