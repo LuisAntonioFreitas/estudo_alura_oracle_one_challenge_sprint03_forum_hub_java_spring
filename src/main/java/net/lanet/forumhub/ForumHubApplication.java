5b29c2c0ee4cfec36614ed8f30352d8f39159e8a
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2024-06-24 21:04:03 -0300

    definindo informações no console sobre inicialização da aplicação

--- src/main/java/net/lanet/forumhub/ForumHubApplication.java
+++ src/main/java/net/lanet/forumhub/ForumHubApplication.java
@@ -1,15 +1,29 @@
 package net.lanet.forumhub;
 
+import net.lanet.forumhub.infra.utilities.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "net.lanet.forumhub")
 public class ForumHubApplication {
+	@Autowired
+	private ApplicationProperties ap; //Status
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumHubApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			// Status
+			ap.status("console");
+		};
 	}
 
 }
