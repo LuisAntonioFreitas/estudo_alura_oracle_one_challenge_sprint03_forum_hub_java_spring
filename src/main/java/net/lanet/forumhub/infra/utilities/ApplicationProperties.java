9f2b1c731e5de7064a2eca85801e6a2440c4a10b
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2024-06-25 23:29:28 -0300

    refatorando

--- src/main/java/net/lanet/forumhub/infra/utilities/ApplicationProperties.java
+++ src/main/java/net/lanet/forumhub/infra/utilities/ApplicationProperties.java
@@ -1,6 +1,6 @@
 package net.lanet.forumhub.infra.utilities;
 
-import net.lanet.forumhub.domain.sys.ISysDbService;
+import net.lanet.forumhub.domain.sysdb.ISysDbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
